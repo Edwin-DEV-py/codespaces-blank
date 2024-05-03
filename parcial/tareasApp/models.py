4d5a253d508a5eacaec0049b986ed95a87a4ef9f
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-05-03 14:18:41 +0000

    db

--- parcial/tareasApp/models.py
+++ parcial/tareasApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class TareaModel(models.Model):
+
+    name = models.CharField(max_length=20)
+    desc = models.CharField(max_length=50)
+
+    createdAt = models.DateTimeField(auto_now=True)
