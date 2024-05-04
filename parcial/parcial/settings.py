0ffa4dac1fb18c348f5fb7783d6961e45875557e
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-05-04 01:35:28 +0000

    conf puerto

--- parcial/parcial/settings.py
+++ parcial/parcial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-b@=ir=it5cwkj^clnc^%cjw7s-hwpucro!k)*)g^p%(bhmr&o1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
